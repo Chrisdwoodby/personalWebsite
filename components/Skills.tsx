274c22319e45bfe1fd79bb30ca43daad1695203a
Chris Woodby <chriswoodby@Chriss-MacBook-Pro.local>
2021-11-20 22:51:31 -0600

    added images to skills page

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,9 +1,20 @@
 import Container from 'react-bootstrap/Container';
+import js from '../dist/js.png';
+import mysql from '../dist/mysql.png';
+import react from '../dist/react.png';
+import node from '../dist/node.png';
+import typescript from '../dist/typescript.png';
+import Row from 'react-bootstrap/Row';
 
 export default function Skills() {
   return (
-      <Container id="Container">
+      <div id="skills">
         <h1>Skills</h1>
-      </Container>
+        <img id="skill" src={js}/>
+        <img id="skill" src={typescript}/>
+        <img id="skill" src={react}/>
+        <img id="skill" src={node}/>
+        <img id="skill" src={mysql}/>
+      </div>
   )
 }
