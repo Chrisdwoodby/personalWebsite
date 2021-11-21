085a6e352480a8da88d25d9af0d9b4e97b31cc56
Chris Woodby <chriswoodby@Chriss-MacBook-Pro.local>
2021-11-20 23:03:52 -0600

    updated contact page with details

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,9 +5,13 @@ export default function Contact() {
     <Container id="Container">
         <h1>contact</h1>
         <h2>LinkedIn</h2>
+        <p>https://www.linkedin.com/in/christopher-woodby-766a88202/</p>
         <h2>email</h2>
+        <p>chrisdwoodby@gmail.com</p>
         <h2>github</h2>
+        <p>https://github.com/Chrisdwoodby</p>
         <h2>phone</h2>
+        <p>512-745-3863</p>
     </Container>
   )
 }
