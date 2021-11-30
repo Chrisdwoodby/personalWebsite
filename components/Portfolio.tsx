e4a89f40d74823346db48e43efd31273672252bb
Christopher Woodby <chrisdwoodby@gmail.com>
2021-11-30 13:14:13 -0600

    Update Portfolio.tsx

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,9 +1,10 @@
 import Row from 'react-bootstrap/Row';
 import flight from '../dist/public/icons/flight.png';
-// import flighttracker from '../dist/flighttracker.mov';
+import flighttracker from '../dist/flight.mov';
 import movies from '../dist/public/icons/movies.png';
 import movie from '../dist/public/icons/nebulamovies.mov';
 import transit from '../dist/public/icons/transit.png';
+import bus from '../dist/public/icons/transit.mov';
 import Col from 'react-bootstrap/Col';
 
 export default function Portfolio() {
@@ -14,6 +15,9 @@ export default function Portfolio() {
         <Row id="project-name">
             <h3>Flight Tracker</h3>
             <p id="techs">React | JavaScript | MySQL | Bootstrap | Webpack | API's</p>
+            <p>Flight tracker which is capable of displaying real time flight data while
+              also providing the user with a hashed and secure log in/sign up.
+            </p>
         </Row>
         <Row id="project-name">
           <div id="main">
@@ -22,7 +26,7 @@ export default function Portfolio() {
                 <img id="test" src={flight}/>
               </div>
               <div id="back">
-                {/* <video id="preview"  playsInline loop autoPlay muted type="video/mp4" src={flighttracker}/> */}
+                <video id="preview"  playsInline loop autoPlay muted type="video/mp4" src={flighttracker}/>
               </div>
             </div>
           </div>
@@ -31,7 +35,8 @@ export default function Portfolio() {
         <Col id="col">
         <Row id="project-name">
           <h3>Nebula Movies</h3>
-          <p id="techs">React | Javascript | Bootstrap | Webpack</p>
+          <p id="techs">React | Javascript | API's | Webpack | Babel</p>
+          <p>Single page movie data application which is capable of sorting and organizing GET requests based on user input.</p>
         </Row>
         <Row id="project-name">
           <div id="main">
@@ -49,7 +54,8 @@ export default function Portfolio() {
         <Col id="col">
         <Row id="project-name">
           <h3>Transit Management</h3>
-          <p id="techs">React | Javascript | Bootstrap | Webpack</p>
+          <p id="techs">React | Javascript | Bootstrap | Webpack | Babel | AWS</p>
+          <p>Web presence for a local consulting business utilizing AWS EC2 and Route 53.</p>
         </Row>
         <Row id="project-name">
           <div id="main">
@@ -58,7 +64,7 @@ export default function Portfolio() {
                 <img id="test" src={transit}/>
               </div>
               <div id="back">
-                about transit management
+                 <video id="preview" playsInline loop autoPlay muted type="video/mp4" src={bus}/>
               </div>
             </div>
           </div>
